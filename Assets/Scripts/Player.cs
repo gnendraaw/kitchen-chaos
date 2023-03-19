70d377dd0343feae9003a674352520709e8b5d3f
gnendraaw <itsgnendraaw@gmail.com>
2023-03-19 14:29:30 +0800

    add unity new input system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,31 +5,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
 
     private bool isWalking;
 
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = 1f;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1f;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = 1f;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1f;
-        }
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementInputNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         isWalking = moveDir != Vector3.zero;
