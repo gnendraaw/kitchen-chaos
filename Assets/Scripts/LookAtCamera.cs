fe2425bf8a3a008b57be0d1f684589614df58db8
gnendraaw <itsgnendraaw@gmail.com>
2023-04-04 16:30:22 +0800

    add LookAtCamera script

---
+++ Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour {
+    private enum Mode {
+        lookAt,
+        lookAtInverted,
+        cameraForward,
+        cameraForwardInverted,
+    }
+
+    [SerializeField] private Mode mode;
+
+    private void LateUpdate() {
+        switch(mode) {
+            case Mode.lookAt:
+                transform.LookAt(Camera.main.transform);
+                break;
+
+            case Mode.lookAtInverted:
+                Vector3 dirFromCamera = transform.position - Camera.main.transform.position;
+                transform.LookAt(dirFromCamera);
+                break;
+
+            case Mode.cameraForward:
+                transform.forward = Camera.main.transform.forward;
+                break;
+
+            case Mode.cameraForwardInverted:
+                transform.forward = -Camera.main.transform.forward;
+                break;
+        }
+    }
+}
+
