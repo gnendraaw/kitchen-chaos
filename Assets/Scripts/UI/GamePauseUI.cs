325961b52fe7278298918de59c3cacb1d66e356c
gnendraaw <itsgnendraaw@gmail.com>
2023-04-19 20:11:54 +0800

    add Options Menu
    Game now have Options Menu. Currently, Options menu can handle just audio level (sfx & bgm volume).
    Build by adding new Options UI canvas element, and some new scripts to handle volume change logic

--- Assets/Scripts/UI/GamePauseUI.cs
+++ Assets/Scripts/UI/GamePauseUI.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class GamePauseUI : MonoBehaviour {
     [SerializeField] private Button mainMenuButton;
     [SerializeField] private Button resumeButton; 
+    [SerializeField] private Button optionsButton;
 
     private void Awake() {
         mainMenuButton.onClick.AddListener(() => {
@@ -16,6 +17,10 @@ public class GamePauseUI : MonoBehaviour {
 
         resumeButton.onClick.AddListener(() => {
             KitchenGameManager.Instance.TogglePauseGame();
+        });
+
+        optionsButton.onClick.AddListener(() => {
+            OptionsUI.Instance.Show();
         });
     }
 
