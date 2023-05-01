65aece01c8f5cc63dbc9e7a2d261bcba948e1198
gnendraaw <itsgnendraaw@gmail.com>
2023-05-01 15:10:37 +0800

    add DeliveryResultUI animation

--- Assets/Scripts/UI/DeliveryResultUI.cs
+++ Assets/Scripts/UI/DeliveryResultUI.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class DeliveryResultUI : MonoBehaviour {
+    private const string POPUP = "Popup";
+
     [SerializeField] private Image backgroundImage;
     [SerializeField] private Image iconImage;
     [SerializeField] private TextMeshProUGUI messageText;
@@ -13,18 +15,30 @@ public class DeliveryResultUI : MonoBehaviour {
     [SerializeField] private Sprite successSprite;
     [SerializeField] private Sprite failedSprite;
 
+    private Animator animator;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
     private void Start() {
         DeliveryManager.Instance.OnRecipeSuccess += DeliveryManager_OnRecipeSuccess;
         DeliveryManager.Instance.OnRecipeFailed += DeliveryManager_OnRecipeFailed;
+
+        gameObject.SetActive(false);
     }
 
     private void DeliveryManager_OnRecipeFailed(object sender, System.EventArgs e) {
+        gameObject.SetActive(true);
+        animator.SetTrigger(POPUP);
         backgroundImage.color = failedColor;
         iconImage.sprite = failedSprite;
         messageText.text = "DELIVERY\nFAILED";
     }
 
     private void DeliveryManager_OnRecipeSuccess(object sender, System.EventArgs e) {
+        gameObject.SetActive(true);
+        animator.SetTrigger(POPUP);
         backgroundImage.color = successColor;
         iconImage.sprite = successSprite;
         messageText.text = "DELIVERY\nSUCCESS";
