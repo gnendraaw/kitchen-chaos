e61689244bf1fdaeee4389eef9cb0410bd5932a8
gnendraaw <itsgnendraaw@gmail.com>
2023-05-01 14:31:38 +0800

    add DeliveryManager Result UI

---
+++ Assets/Scripts/UI/DeliveryResultUI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeliveryResultUI : MonoBehaviour {
+    [SerializeField] private Image backgroundImage;
+    [SerializeField] private Image iconImage;
+    [SerializeField] private TextMeshProUGUI messageText;
+    [SerializeField] private Color successColor;
+    [SerializeField] private Color failedColor;
+    [SerializeField] private Sprite successSprite;
+    [SerializeField] private Sprite failedSprite;
+
+    private void Start() {
+        DeliveryManager.Instance.OnRecipeSuccess += DeliveryManager_OnRecipeSuccess;
+        DeliveryManager.Instance.OnRecipeFailed += DeliveryManager_OnRecipeFailed;
+    }
+
+    private void DeliveryManager_OnRecipeFailed(object sender, System.EventArgs e) {
+        backgroundImage.color = failedColor;
+        iconImage.sprite = failedSprite;
+        messageText.text = "DELIVERY\nFAILED";
+    }
+
+    private void DeliveryManager_OnRecipeSuccess(object sender, System.EventArgs e) {
+        backgroundImage.color = successColor;
+        iconImage.sprite = successSprite;
+        messageText.text = "DELIVERY\nSUCCESS";
+    }
+}
+
