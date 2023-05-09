d8010407e973c3506c2a806f6e21a330d8f6f483
gnendraaw <itsgnendraaw@gmail.com>
2023-05-10 00:43:12 +0800

    Sync Pickup Objects

---
+++ Assets/Scripts/KitchenGameMultiplayer.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class KitchenGameMultiplayer : NetworkBehaviour {
+    public static KitchenGameMultiplayer Instance { get; private set; }
+
+    [SerializeField] private KitchenObjectListSO kitchenObjectListSO;
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    public  void SpawnKitchenObject(KitchenObjectSO kitchenObjectSO, IKitchenObjectParent kitchenObjectParent) {
+        SpawnKitchenObjectServerRpc(GetKitchenObjectSOListIndex(kitchenObjectSO), kitchenObjectParent.GetNetworkObject());
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void SpawnKitchenObjectServerRpc(int kitchenObjectSOListIndex, NetworkObjectReference kitchenObjectParentNetworkReference) {
+        KitchenObjectSO kitchenObjectSO = GetKitchenObjectSOFromIndex(kitchenObjectSOListIndex);
+
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+
+        NetworkObject kitchenObjectNetworkObject = kitchenObjectTransform.GetComponent<NetworkObject>();
+        kitchenObjectNetworkObject.Spawn(true);
+
+        KitchenObject kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+        kitchenObjectParentNetworkReference.TryGet(out NetworkObject kitchenObjectParentNetworkObject);
+        IKitchenObjectParent kitchenObjectParent = kitchenObjectParentNetworkObject.GetComponent<IKitchenObjectParent>();
+
+        kitchenObject.SetkitchenObjectParent(kitchenObjectParent);
+    }
+
+    private int GetKitchenObjectSOListIndex(KitchenObjectSO kitchenObjectSO) {
+        return kitchenObjectListSO.kitchenObjectSOList.IndexOf(kitchenObjectSO);
+    }
+
+    private KitchenObjectSO GetKitchenObjectSOFromIndex(int index) {
+        return kitchenObjectListSO.kitchenObjectSOList[index];
+    }
+}
+
--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class KitchenObject : MonoBehaviour
+public class KitchenObject : NetworkBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
@@ -29,8 +30,8 @@ public class KitchenObject : MonoBehaviour
 
         kitchenObjectParent.SetKitchenObject(this);
 
-        transform.parent = kitchenObjectParent.GetKitchenObjectFollowTransform();
-        transform.localPosition = Vector3.zero;
+        // transform.parent = kitchenObjectParent.GetKitchenObjectFollowTransform();
+        // transform.localPosition = Vector3.zero;
     }
 
     public IKitchenObjectParent GetkitchenObjectParent()
@@ -54,13 +55,7 @@ public class KitchenObject : MonoBehaviour
         }
     }
 
-    public static KitchenObject SpawnKitchenObject(KitchenObjectSO kitchenObjectSO, IKitchenObjectParent kitchenObjectParent) {
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
-
-        KitchenObject kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
-
-        kitchenObject.SetkitchenObjectParent(kitchenObjectParent);
-
-        return kitchenObject;
+    public static void SpawnKitchenObject(KitchenObjectSO kitchenObjectSO, IKitchenObjectParent kitchenObjectParent) {
+        KitchenGameMultiplayer.Instance.SpawnKitchenObject(kitchenObjectSO, kitchenObjectParent);
     }
 }
