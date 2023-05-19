6a9679dfee299a2ce08405c81d0309de470e3aae
gnendraaw <itsgnendraaw@gmail.com>
2023-05-19 21:37:40 +0800

    Sync Game State

---
+++ Assets/Scripts/UI/WaitingForOtherPlayersUI.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaitingForOtherPlayersUI : MonoBehaviour {
+    private void Start() {
+        KitchenGameManager.Instance.OnLocalPlayerReadyChanged += KitchenGameManager_OnLocalPlayerReadyChanged;
+        KitchenGameManager.Instance.OnStateChanged += KitchenGameManager_OnStateChanged;
+
+        Hide();
+    }
+
+    private void KitchenGameManager_OnStateChanged(object sender, System.EventArgs e) {
+        if (KitchenGameManager.Instance.IsCountdownToStartActive()) {
+            Hide();
+        }
+    }
+
+    private void KitchenGameManager_OnLocalPlayerReadyChanged(object sender, System.EventArgs e) {
+        if (KitchenGameManager.Instance.IsLocalPlayerReady()) {
+            Show();
+        }
+    }
+
+    private void Hide() {
+        gameObject.SetActive(false);
+    }
+
+    private void Show() {
+        gameObject.SetActive(true);
+    }
+}
+
