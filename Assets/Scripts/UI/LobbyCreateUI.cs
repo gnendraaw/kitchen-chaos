cf7f1c157a0053f2fbe5842307f425ed686bef79
gnendraaw <itsgnendraaw@gmail.com>
2023-06-23 20:00:04 +0800

    Remove Single Player Mode

--- Assets/Scripts/UI/LobbyCreateUI.cs
+++ Assets/Scripts/UI/LobbyCreateUI.cs
@@ -30,6 +30,8 @@ public class LobbyCreateUI : MonoBehaviour {
 
     public void Show() {
         gameObject.SetActive(true);
+
+        createPrivateButton.Select();
     }
 
     private void Hide() {
--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -5,20 +5,11 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour {
-    [SerializeField] private Button playMultiplayerButton;
-    [SerializeField] private Button playSinglePlayerButton;
+    [SerializeField] private Button playButton;
     [SerializeField] private Button quitButton;
 
     private void Awake() {
-        playMultiplayerButton.onClick.AddListener(() => {
-            KitchenGameMultiplayer.playMultiplayer = true;
-
-            Loader.Load(Loader.Scene.LobbyScene);
-        });
-
-        playSinglePlayerButton.onClick.AddListener(() => {
-            KitchenGameMultiplayer.playMultiplayer = false;
-
+        playButton.onClick.AddListener(() => {
             Loader.Load(Loader.Scene.LobbyScene);
         });
 
